7a0c7a6d01c2925ed50047d6b8010896dd3bd92b
Jim Tryon <tryonjf@gmail.com>
2015-11-18 00:48:04 -0800

    Added regular expression patterns and methods to check against patterns

--- string/operations/StringParsing.java
+++ string/operations/StringParsing.java
@@ -2,14 +2,39 @@ package edu.greenriver.it.string.operations;
 
 public class StringParsing
 {
+	// returns all characters from the start of the string to the
+	// first occurence of the delimiter
 	public static String before(String subject, String delimiter)
 	{
-		return null;
+		// 4
+		int endString = subject.indexOf(delimiter);
+		
+		String subString = null;
+		
+		// check if value exists
+		if (endString != -1)
+		{
+			subString = subject.substring(0, endString);
+			
+		}
+		return subString;
 	}
 	
+	// returns all characters from the delimiter to the end
+	// of the string 
 	public static String after(String subject, String delimiter)
 	{
-		return null;
+		int endString = subject.indexOf(delimiter);
+		int length = subject.length();
+		
+		String subString = null;
+		
+		if (endString != -1)
+		{
+			subString = subject.substring(endString, length);
+		}
+		return subString;
+		
 	}
 
 }
--- string/operations/StringRegex.java
+++ string/operations/StringRegex.java
@@ -1,24 +1,67 @@
 package edu.greenriver.it.string.operations;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringRegex
 {	
 	public static boolean isEmail(String subject)
 	{	
-		return false;		
+		String emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+		
+		Pattern regex = Pattern.compile(emailPattern);
+		
+		Matcher emailMatcher = regex.matcher(subject);
+		
+		if (emailMatcher.matches())
+		{
+			return true;
+		}
+		return false;
 	}
 	
 	public static boolean isZipCode(String subject)
 	{
+		String zipCodePattern = "^\\d{5}(-\\d{4})?$";
+
+		Pattern regex = Pattern.compile(zipCodePattern);
+
+		Matcher zipCodeMatcher = regex.matcher(subject);
+
+		if (zipCodeMatcher.matches())
+		{
+			return true;
+		}
 		return false;
 	}
 	
 	public static boolean isPhoneNumber(String subject)
 	{
+		String phoneNumberPattern = "^(\\d{3}-?\\d{3}-?\\d{4})$";
+
+		Pattern regex = Pattern.compile(phoneNumberPattern);
+
+		Matcher phoneNumberMatcher = regex.matcher(subject);
+
+		if (phoneNumberMatcher.matches())
+		{
+			return true;
+		}
 		return false;
 	}
 	
 	public static boolean isUrl(String subject)
 	{
+		String urlPattern = "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$";
+
+		Pattern regex = Pattern.compile(urlPattern);
+
+		Matcher urlMatcher = regex.matcher(subject);
+
+		if (urlMatcher.matches())
+		{
+			return true;
+		}
 		return false;
 	}
 }
